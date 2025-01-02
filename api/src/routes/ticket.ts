a8e5e02e7a839b2789f84a123310cfcdc26694dd
Esdras Silva <esdrasdev0@gmail.com>
2025-01-02 12:08:50 -0300

    feat: adicionada a rota de criação de ticket no ticketRoutes

--- api/src/routes/ticket.ts
+++ api/src/routes/ticket.ts
@@ -1,17 +1,49 @@
 import { FastifyInstance } from "fastify";
-import {z} from 'zod'
+import {z} from 'zod';
+import dayjs from "dayjs";
+import { prisma } from "../lib/prisma";
+import { log } from "console";
 
 export function ticketRoutes(fastify: FastifyInstance){
     fastify.post('/ticket', async (request, reply) =>{
         const ticketSchema = z.object({
             codeBar: z.string(),
-            expiresIn: z.date(),
+            expiresIn: z.string(),
             paidAt: z.date(),
             enterpriseId: z.string()
-        })
+        });
 
 
-        const {codeBar,expiresIn,paidAt,enterpriseId} = ticketSchema.parse(request.body)
+     
+
+        try{
+
+            const {codeBar,expiresIn,paidAt,enterpriseId} = ticketSchema.parse(request.body);
+
+           if(paidAt){
+            await prisma.ticket.create({
+                data:{
+                    codeBar,
+                    expiresIn: dayjs(expiresIn).toDate(),
+                    enterpriseId,
+                    paidAt: dayjs(paidAt).toDate()
+                }
+            })
+           } else{
+            await prisma.ticket.create({
+                data:{
+                    codeBar,
+                    expiresIn: dayjs(expiresIn).toDate(),
+                    enterpriseId
+                }
+            })
+           }
+
+           return reply.status(201)
+        } catch(e){
+           return reply.status(400)
+           
+        }
 
     })
 }
