f7bf267f81b8d0bd83021946ef9e481ba2dc5e78
Esdras Silva <esdrasdev0@gmail.com>
2025-01-04 16:43:25 -0300

    feat: criada as rotas de tickets

--- api/src/routes/ticket.ts
+++ api/src/routes/ticket.ts
@@ -1,49 +1,78 @@
 import { FastifyInstance } from "fastify";
 import {z} from 'zod';
-import dayjs from "dayjs";
+import dayjs from "../lib/dayjs";
 import { prisma } from "../lib/prisma";
-import { log } from "console";
 
 export function ticketRoutes(fastify: FastifyInstance){
     fastify.post('/ticket', async (request, reply) =>{
         const ticketSchema = z.object({
             codeBar: z.string(),
             expiresIn: z.string(),
-            paidAt: z.date(),
-            enterpriseId: z.string()
+            paidAt: z.string().optional(),
+            enterpriseId: z.string(),
+            value: z.number()
         });
-
-
-     
 
         try{
 
-            const {codeBar,expiresIn,paidAt,enterpriseId} = ticketSchema.parse(request.body);
+            const {codeBar,expiresIn,paidAt,enterpriseId, value} = ticketSchema.parse(request.body);
 
-           if(paidAt){
-            await prisma.ticket.create({
-                data:{
-                    codeBar,
-                    expiresIn: dayjs(expiresIn).toDate(),
-                    enterpriseId,
-                    paidAt: dayjs(paidAt).toDate()
-                }
-            })
-           } else{
-            await prisma.ticket.create({
-                data:{
-                    codeBar,
-                    expiresIn: dayjs(expiresIn).toDate(),
-                    enterpriseId
-                }
-            })
-           }
+            const data: {codeBar: string, paidAt?: Date, expiresIn: Date, enterpriseId: string, value: number} = {
+                codeBar,
+                enterpriseId,
+                value,
+                expiresIn: dayjs(expiresIn).toDate()
+            }
 
-           return reply.status(201)
-        } catch(e){
-           return reply.status(400)
+            if(paidAt) data.paidAt = dayjs(paidAt).toDate()
+
+           await prisma.ticket.create({
+            data
+           })
+        
            
+            reply.status(201)
+        } catch(e){
+            reply.status(400).send(e)
         }
+    })
 
+    fastify.get('/tickets', async (request,reply) =>{
+
+        const tickets = await prisma.ticket.findMany()
+
+        reply.status(200).send({tickets})
+    })
+
+    fastify.put('/ticket/:id', async (request,reply) =>{
+        const paramSchema = z.object({
+            id: z.string()
+        })
+
+        const updateSchema = z.object({
+            paidAt: z.string()
+        })
+
+        const {id} = paramSchema.parse(request.params)
+        const {paidAt} = updateSchema.parse(request.body)
+
+        const isExists = await prisma.ticket.findUnique({
+            where:{
+                id
+            }
+        })
+
+        if (!isExists) reply.status(500)
+
+        await prisma.ticket.update({
+            data:{
+                paidAt: dayjs(paidAt).toDate()
+            },
+            where:{
+                id
+            }
+        })
+
+        reply.status(200)
     })
 }
