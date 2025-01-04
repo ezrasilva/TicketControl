8759eabd2b0e36a389c25a603dc7d40bc2ae4344
Esdras Silva <esdrasdev0@gmail.com>
2025-01-04 16:44:07 -0300

    feat: adcionada as rotas no index

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { ticketRoutes } from './ticket';
+import { enterpriseRoutes } from './enterprise';
+import { reportRoutes } from './reports';
 
 
 export async function registerRoutes(fastify: FastifyInstance) {
   await fastify.register(ticketRoutes);
-
+  await fastify.register(enterpriseRoutes);
+  await fastify.register(reportRoutes)
 }
